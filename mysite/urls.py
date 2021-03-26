a2f7fc71544217203e4841e475226396f674d054
Michiel DeKoninck <michieldekoninck2@gmail.com>
2021-03-26 16:34:10 +0100

    upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+app_name = 'mysite'
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
